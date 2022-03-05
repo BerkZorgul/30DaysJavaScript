addfdc9d6a412eeead2335173546c32ab9ad1ad6
BerkZorgul <berkzorgul05@gmail.com>
2022-03-05 23:37:24 +0300

    Day4

---
+++ day4.js
@@ -0,0 +1,45 @@
+let result = parseInt(prompt ("2 + 5 kaç yapar?", "2 + 5"))
+
+if (result == "7"){
+
+console.log("Doğru cevabı verdin tebrikler!")
+
+}
+else {
+    console.log("Hatalı cevap verdin, doğru cevap 7 olmalıydı!")
+}
+
+console.log(
+    resut == 7 ?"Doğru Cevap" : "Yanlış Cevap"
+
+)
+
+
+
+switch(result) {
+    
+    case "7":
+        console.log("Doğru Cevap - switch")
+    break
+
+    case "5":
+        console.log("Cevap Hatalı Ama AfFerin")
+    break
+
+    default:
+        console.log("Hatalı Cevap - switch")
+
+}
+
+
+let number = 5
+
+if (number == 4){
+//
+} else if (number >= 5){
+//
+} else if (number > 5){
+//
+} else {
+//
+}
