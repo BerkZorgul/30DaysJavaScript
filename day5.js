df6a3bcefc1515e41e3febbecd8aa1fb74e5fdd7
BerkZorgul <berkzorgul05@gmail.com>
2022-03-07 23:03:17 +0300

    30DayJavaScript

---
+++ day5.js
@@ -0,0 +1,50 @@
+// let names = Array(3).fill("Berk")
+
+// console.log(names)
+
+// let names2 = ["Berk" , 15 , 2007 , "Zorgül"]
+// names2[0] = "Ahmet";
+
+// console.log (names2)
+
+// console.log("berk".split(""))
+
+// console.log("Berk".length)
+
+// let array1 = [1, 2, 3,]
+// let array2 = [4, 5, 6,]
+// let array3 = [7, 8, 9,]
+
+// let result = array1.concat(array2, array3, [10, 11, 12])
+
+// console.log(result)
+
+let names = ["Berk", "Volkan", "Gökhan"]
+
+names.pop()
+names.shift()
+
+
+names.unshift("İlk Eleman")
+names.push("Son Eleman")
+
+// console.log(names)
+// console.log( "berk".split("").reverse().join("") )
+
+const numbers = [1,7,8,2,6,5]
+console.log(numbers)
+
+numbers.sort()
+console.log(numbers)
+
+
+//  let names2 = "berk".split("")
+
+// console.log(Array.isArray(names2))
+
+// console.log(Array.isArray(names2))
+
+// let currentNames = names.splice(1, 2, "Mehmet2", "Tayfun2")
+
+// console.log(currentNames)
+// console.log(names)
