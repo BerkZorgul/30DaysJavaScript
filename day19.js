cc175b9d73f8a81910fb70c59b25d26484dff974
Berk Zorgül <86024585+BerkZorgul@users.noreply.github.com>
2023-01-28 13:17:12 +0300

    day19

---
+++ day19.js
@@ -0,0 +1,48 @@
+/*
+function outerFunction() {
+    let count = 0;
+    function innerFunction() {
+        count++
+        return count
+    }
+
+    return innerFunction
+}
+const innerFunc = outerFunction()
+
+console.log(innerFunc())
+console.log(innerFunc())
+console.log(innerFunc())
+*/
+
+function counter(){
+    let count = 0;
+    function up() {
+        count++
+        return count
+    }
+    function down() {
+        count--
+        return count
+    }
+
+    return {
+        yukari: up,
+        asagi: down
+    }
+}
+
+let count = counter()
+
+console.log(
+    count.yukari()
+)
+console.log(
+    count.yukari()
+)
+console.log(
+    count.yukari()
+)
+console.log(
+    count.asagi()
+)
